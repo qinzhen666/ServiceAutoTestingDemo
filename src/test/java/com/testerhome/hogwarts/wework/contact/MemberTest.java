4c346f860bc807503e6b574c41244a0898e29d16
qinzhen <376057520@qq.comgit config --global user.email 376057520@qq.com>
2019-06-05 14:50:23 +0800

    read function

--- src/test/java/com/testerhome/hogwarts/wework/contact/MemberTest.java
+++ src/test/java/com/testerhome/hogwarts/wework/contact/MemberTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberTest {
 
-    Random ra = new Random();
+    static Random ra = new Random();
     static Member member;
     @BeforeEach
     void setUp() {
@@ -32,5 +32,27 @@ class MemberTest {
         map.put("mobile",151 + member.random.substring(0,7) + ra.nextInt(9));
         map.put("email",member.random.substring(0,7)+ ra.nextInt(9) + "@qq.com");
         member.create(map).then().statusCode(200).body("errcode",equalTo(0));
+    }
+
+    @Test
+    static HashMap createForRead(String name){
+        String nameNew = name + member.random;
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("userid",nameNew);
+        map.put("name",nameNew);
+        map.put("department", Arrays.asList(1,2));
+        map.put("mobile",151 + member.random.substring(0,7) + ra.nextInt(9));
+        map.put("email",member.random.substring(0,7)+ ra.nextInt(9) + "@qq.com");
+        return map;
+    }
+
+    @Test
+    void read() {
+        //path("department.find{it.name=='"+ oldName +"'}.id").toString();
+        HashMap map = MemberTest.createForRead("readUser_");
+        member.create(map);
+        String userid = String.valueOf(map.get("userid"));
+        System.out.println("userID是" + userid);
+        member.read(userid).then().statusCode(200).body("errcode",equalTo(0));
     }
 }
